5e69a4c4c28af718bb6e50450b8140157a19e47c
jlarockk <33941219+jlarockk@users.noreply.github.com>
2018-01-30 14:05:35 -0500

    Update EasyCMD.bat

--- EasyCMD.bat
+++ EasyCMD.bat
@@ -3,6 +3,7 @@ title Easy CMD
 
 :start
 cls
+
 echo.
 echo *{EasyCMD}* User: %username%
 echo ***************************
@@ -13,28 +14,33 @@ echo [3] Help
 echo [4] Info
 echo.
 echo ***************************
+
 set /p choice= What would you like to do?: 
 
 if %choice%==1 goto main
 if %choice%==2 goto startexit
 if %choice%==3 goto help
 if %choice%==4 goto info
+
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
+
 pause
 goto start
 
-
 :info
 cls
+
 echo.
-echo {EasyCMD Info}
+echo {EasyCMD Info} User: %username%
 echo ***************************
 echo.
 echo Coded by Justin
 echo I did not create the Cleanup Utility!
 echo.
+echo ***************************
+
 pause
 goto start
 
@@ -44,14 +50,32 @@ set /p YN=Are you sure you want to exit? (y/n):
 
 if %YN%==y exit
 if %YN%==n goto start
+
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
+
 pause
 goto startexit
 
+:help
+cls
+
+echo.
+echo {EasyCMD Help} User: %username%
+echo ********************
+echo.
+echo [1] Type the number of the option and press Enter
+echo [2] Ctrl+C cancels a running command
+echo.
+echo ********************
+
+pause
+goto start
+
 :main
 cls
+
 echo.
 echo {Main Menu} User: %username%
 echo ***************************
@@ -60,7 +84,7 @@ echo [1] Networking Commands
 echo [2] Disk Cleanup
 echo [3] Check Disk
 echo [4] System Information
-echo [5] Run Command Prompt
+echo [5] Open Command Prompt
 echo [6] Back
 echo [7] Exit
 echo.
@@ -80,24 +104,29 @@ if %choice%==7 goto exit
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
+
 pause
 goto main
 
 :cmd
 echo.
-set /p choice=Are you sure you want to exit EasyCMD and run Command Prompt? (y/n): 
+set /p choice=Are you sure you want to exit EasyCMD and open Command Prompt? (y/n): 
+
 if %choice%==y (cls
 cd C:/
 cmd)
 if %choice%==n goto main
+
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
+
 pause
 goto cmd
 
 :clean
 cls
+
 echo.
 echo {Disk Cleanup} User: %username%
 echo *************************************
@@ -111,6 +140,7 @@ echo [6] Exit
 echo.
 echo *************************************
 echo.
+
 set /p op=Run: 
 if %op%==1 goto 1
 if %op%==2 goto 2
@@ -122,11 +152,11 @@ if %op%==6 goto cleanexit
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
+
 pause
 goto clean
 
 :cleanexit
-
 echo.
 set /p YN=Are you sure you want to exit? (y/n): 
 
@@ -136,19 +166,24 @@ if %YN%==n goto clean
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
+
 pause
 goto cleanexit
 
 :1
 cls
+
 echo --------------------------------------------------------------------------------
 echo Delete Internet Cookies
 echo --------------------------------------------------------------------------------
 echo.
 echo Deleting Cookies...
+
 ping localhost -n 3 >nul
+
 del /f /q "%userprofile%\Cookies\*.*"
 cls
+
 echo --------------------------------------------------------------------------------
 echo Delete Internet Cookies
 echo --------------------------------------------------------------------------------
@@ -156,6 +191,7 @@ echo.
 echo Cookies deleted.
 echo.
 echo Press any key to return to the menu. . .
+
 pause >nul
 goto clean
 
@@ -166,9 +202,11 @@ echo Delete Temporary Internet Files
 echo --------------------------------------------------------------------------------
 echo.
 echo Deleting Temporary Files...
+
 ping localhost -n 3 >nul
 del /f /q "%userprofile%\AppData\Local\Microsoft\Windows\Temporary Internet Files\*.*"
 cls
+
 echo --------------------------------------------------------------------------------
 echo Delete Temporary Internet Files
 echo --------------------------------------------------------------------------------
@@ -176,6 +214,7 @@ echo.
 echo Temporary Internet Files deleted.
 echo.
 echo Press any key to return to the menu. . .
+
 pause >nul
 goto clean
 
@@ -186,7 +225,9 @@ echo Disk Cleanup
 echo --------------------------------------------------------------------------------
 echo.
 echo Running Disk Cleanup...
+
 ping localhost -n 3 >nul
+
 if exist "C:\WINDOWS\temp"del /f /q "C:WINDOWS\temp\*.*"
 if exist "C:\WINDOWS\tmp" del /f /q "C:\WINDOWS\tmp\*.*"
 if exist "C:\tmp" del /f /q "C:\tmp\*.*"
@@ -243,47 +284,49 @@ goto clean
 
 :4
 cls
+
 echo --------------------------------------------------------------------------------
 echo Disk Defragment
 echo --------------------------------------------------------------------------------
 echo.
 echo Defragmenting hard disks...
+
 ping localhost -n 3 >nul
 defrag -c -v
 cls
+
 echo --------------------------------------------------------------------------------
 echo Disk Defragment
 echo --------------------------------------------------------------------------------
 echo.
 echo Disk Defrag successful!
 echo.
+
 pause
 goto clean
 
-
 :network
 cls
+
 echo.
 echo {Networking Commands} User: %username%
 echo ***************************
 echo.
-echo [1] ping
-echo [2] ipconfig
-echo [3] ipconfig /all
-echo [4] ipconfig /release
-echo [5] ipconfig /renew
-echo [6] tracert
-echo [7] netstat
-echo [8] pathping
-echo [9] nslookup
+echo [1] test a connection
+echo [2] display network settings currently assigned
+echo [3] display all TCP/IP network configuration values
+echo [4] force a client to give up lease
+echo [5] renew IP address
+echo [6] record route of a connection to its destination
+echo [7] display protocol statistics and current TCP/IP connections
+echo [8] provide info on network latency and loss
+echo [9] display info to diagnose DNS infastructure
 echo [10] Exit
 echo [11] Back
-echo [12] Help
 echo.
 echo ***************************
 
 set /p choice=What would you like to do?: 
-
 
 if %choice%==1 goto ping
 if %choice%==2 goto ipconfig
@@ -296,16 +339,15 @@ if %choice%==8 goto pathping
 if %choice%==9 goto nslookup
 if %choice%==10 goto netexit
 if %choice%==11 goto main
-if %choice%==12 goto nethelp
 
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
+
 pause
 goto network
 
 :netexit
-
 echo.
 set /p YN=Are you sure you want to exit? (y/n): 
 
@@ -315,30 +357,9 @@ if %YN%==n goto network
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
+
 pause
 goto netexit
-
-
-:nethelp
-cls
-echo.
-echo {Networking Commands Help}
-echo ***************************
-echo.
-echo [1] ping tests a connection
-echo [2] ipconfig displays network settings currently assigned
-echo [4] ipconfig /all displays all TCP/IP network configuration values
-echo [5] ipconfig /release forces client to give up lease
-echo [6] ipconfig /renew renew IP address
-echo [7] tracert records route of a connection to its destination
-echo [8] netstat displays protocol statistics and current TCP/IP connections
-echo [9] pathping provides info on network latency and loss
-echo [10] nslookup displays info to diagnose DNS infastructure
-echo.
-echo ***************************
-pause
-goto network
-
 
 :ping
 cls
@@ -359,10 +380,13 @@ goto ping
 :ipconfig
 cls 
 ipconfig
+
 echo.
 echo [+] Done
 echo.
+
 pause
+
 echo.
 set /p doagain=Would you like to run this command again? (y/n): 
 if %doagain%==y goto ipconfig
@@ -370,14 +394,16 @@ if %doagain%==n goto network
 
 goto network
 
-
 :ipconfig /all
 cls
 "ipconfig /all"
+
 echo.
 echo [+] Done
 echo.
+
 pause
+
 echo.
 set /p doagain=Would you like to run this command again? (y/n): 
 if %doagain%==y goto ipconfig /all
@@ -388,10 +414,13 @@ goto network
 :ipconfig /release
 cls
 "ipconfig /release"
+
 echo.
 echo [+] Done
 echo.
+
 pause
+
 echo.
 set /p doagain=Would you like to run this command again? (y/n): 
 if %doagain%==y goto ipconfig /release
@@ -402,10 +431,13 @@ goto network
 :ipconfig /renew
 cls
 "ipconfig /renew"
+
 echo.
 echo [+] Done
 echo.
+
 pause
+
 echo.
 set /p doagain=Would you like to run this command again? (y/n): 
 if %doagain%==y goto ipconfig /renew
@@ -416,10 +448,13 @@ goto network
 :chkdsk
 cls
 chkdsk
+
 echo.
 echo [+] Done
 echo.
+
 pause
+
 echo.
 set /p doagain=Would you like to run this command again? (y/n): 
 if %doagain%==y goto Chkdsk
@@ -429,12 +464,16 @@ goto check
 
 :tracert
 cls
+
 set /p tracertchoice=Enter an IP or WEB address: 
 tracert %tracertchoice%
+
 echo.
 echo [+] Done
 echo.
+
 pause
+
 echo.
 set /p doagain=Would you like to run this command again? (y/n): 
 if %doagain%==y goto tracert
@@ -445,10 +484,13 @@ goto network
 :netstat
 cls
 netstat
+
 echo.
 echo [+] Done
 echo.
+
 pause
+
 echo.
 set /p doagain=Would you like to run this command again? (y/n): 
 if %doagain%==y goto netstat
@@ -459,10 +501,13 @@ goto network
 :systeminfo
 cls
 systeminfo
+
 echo.
 echo [+] Done
 echo.
+
 pause
+
 echo.
 set /p doagain=Would you like to run this command again? (y/n): 
 if %doagain%==y goto systeminfo
@@ -473,10 +518,13 @@ goto stuff
 :driverquery
 cls
 driverquery
+
 echo.
 echo [+] Done
 echo.
+
 pause
+
 echo.
 set /p doagain=Would you like to run this command again? (y/n): 
 if %doagain%==y goto driverquery
@@ -486,12 +534,16 @@ goto stuff
 
 :pathping
 cls
+
 set /p pathpingchoice=What would you like test? (IP or WEB address) 
 pathping %pathpingchoice%
+
 echo.
 echo [+] Done
 echo.
+
 pause
+
 echo.
 set /p doagain=Would you like to run this command again? (y/n): 
 if %doagain%==y goto pathping
@@ -502,10 +554,13 @@ goto network
 :nslookup
 cls
 nslookup
+
 echo.
 echo [+] Done
 echo.
+
 pause
+
 echo.
 set /p doagain=Would you like to run this command again? (y/n): 
 if %doagain%==y goto nslookup
@@ -513,22 +568,9 @@ if %doagain%==n goto network
 
 goto network
 
-:getmac
+:system
 cls
-getmac
-echo.
-echo [+] Done
-echo.
-pause
-echo.
-set /p doagain=Would you like to run this command again? (y/n): 
-if %doagain%==y goto getmac
-if %doagain%==n goto stuff
 
-goto stuff
-
-:stuff
-cls
 echo.
 echo {System Information} User: %username%
 echo ***************************
@@ -546,16 +588,15 @@ if %stuffchoice%==1 goto getmac
 if %stuffchoice%==2 goto systeminfo
 if %stuffchoice%==3 goto driverquery
 if %stuffchoice%==4 goto main
-if %stuffchoice%==5 goto stuffexit
+if %stuffchoice%==5 goto systemexit
 
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
 pause
-goto stuff
+goto system
 
-:stuffexit
-
+:systemexit
 echo.
 set /p YN=Are you sure you want to exit? (y/n): 
 
@@ -568,8 +609,26 @@ echo.
 pause
 goto stuffexit
 
+:getmac
+cls
+getmac
+
+echo.
+echo [+] Done
+echo.
+
+pause
+
+echo.
+set /p doagain=Would you like to run this command again? (y/n): 
+if %doagain%==y goto getmac
+if %doagain%==n goto system
+
+goto system
+
 :check
 cls
+
 echo.
 echo {Check Disk} User: %username%
 echo ***************************
@@ -588,11 +647,11 @@ if %checkc%==3 goto checkexit
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
+
 pause
 goto check
 
 :checkexit
-
 echo.
 set /p YN=Are you sure you want to exit? (y/n): 
 
@@ -602,11 +661,11 @@ if %YN%==n goto check
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
+
 pause
 goto checkexit
 
 :exit
-
 echo.
 set /p YN=Are you sure you want to exit? (y/n): 
 
@@ -616,19 +675,6 @@ if %YN%==n goto main
 echo.
 echo [!] Error. Please enter a valid option.
 echo.
+
 pause
 goto exit
-
-:help
-
-cls
-echo.
-echo {Help}
-echo ********************
-echo.
-echo [1] Type the name or number of the option you want to select
-echo [2] Ctrl+C cancels a running command.
-echo.
-echo ********************
-pause
-goto start
